539ddd80442c8b7f0d7657d25ce994727b4314ad
bartekvs08_ <sade79dev@gmail.com>
2026-08-12 18:49:56 +0200

    feat: create sign-in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,80 @@
 'use client';
 
-export default function SignInPage() {}
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { Field, FieldError, FieldGroup, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const signInSchema = z.object({
+  email: z.email('Invalid email'),
+  password: z.string().nonempty('Password is required')
+});
+
+export default function SignInPage() {
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  function onSubmit(data: z.infer<typeof signInSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <form id="sign-in-form" onSubmit={form.handleSubmit(onSubmit)}>
+      <FieldGroup>
+        <Controller
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="Wpisz adres email..."
+                type="email"
+                id="email"
+                {...field}
+              />
+              {fieldState.invalid ? <FieldError errors={[fieldState.error]} /> : null}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="password">Hasło</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="Wpisz hasło..."
+                type="password"
+                id="password"
+                {...field}
+              />
+              {fieldState.invalid ? <FieldError errors={[fieldState.error]} /> : null}
+            </Field>
+          )}
+        />
+      </FieldGroup>
+
+      <Field orientation="horizontal">
+        <Button type="button" variant="outline" onClick={() => form.reset()}>
+          Wyczyść
+        </Button>
+
+        <Button type="submit" form="sign-in-form">
+          {form.formState.isSubmitting ? 'Logowanie...' : 'Zaloguj się'}
+        </Button>
+      </Field>
+    </form>
+  );
+}
