f9bde4c47880fdb19f75de1986be05f4a11d9e68
bartekvs08_ <sade79dev@gmail.com>
2026-08-13 20:12:23 +0200

    fix: change baseUrl env variable to client side

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,8 +7,10 @@ export const env = createEnv({
     DB_USER: z.string().min(1),
     DB_PASSWORD: z.string().optional(),
     DB_DATABASE: z.string().min(1),
-    BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.string().min(1)
+    BETTER_AUTH_SECRET: z.string().min(1)
+  },
+  client: {
+    NEXT_PUBLIC_BETTER_AUTH_URL: z.string().min(1)
   },
   runtimeEnv: {
     DB_HOST: process.env.DB_HOST,
@@ -16,6 +18,6 @@ export const env = createEnv({
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_DATABASE: process.env.DB_DATABASE,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL
+    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL
   }
 });
